0649d30eeaa0c2ab21dbb0cd1a1bd5b2cf286a3b
Afnan Aljohani <i2fnaaljohani@gmail.com>
2026-06-28 21:20:28 +0000

    Add tasks 8, 9, 12

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a function to convert a class to a JSON dict."""
+
+
+def class_to_json(obj):
+    """Return the dictionary description for JSON serialization of an object."""
+    return obj.__dict__
